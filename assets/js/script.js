3f8b846a0d77a180cb23347064894c5aaa6a6a2a
mayaj0yce <m.j.thayne@gmail.com>
2023-05-18 18:11:15 -0600

    the clean up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,17 @@
-$(document).ready(function () {// essentially tells engine to load 1)html & 2)css first.
-  //display current day & time.
+$(document).ready(function () {
+
   $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
-  //Assign saveBtn click listener for user input and time stamp??
+
   $(".saveBtn").on("click", function () {
-      //get nearby values.
+      
       console.log(this);
       var text = $(this).siblings(".description").val();
       var time = $(this).parent().attr("id");
 
-      //set items in local storage.
       localStorage.setItem(time, text);
   })
-  //load any saved data from LocalStorage - do this for each hour created.
+  
   $("#hour9 .description").val(localStorage.getItem("hour9"));
   $("#hour10 .description").val(localStorage.getItem("hour10"));
   $("#hour11 .description").val(localStorage.getItem("hour11"));
@@ -25,16 +24,14 @@ $(document).ready(function () {// essentially tells engine to load 1)html & 2)cs
 
 
   function hourTracker() {
-      //get current number of hours.
+      
       var currentHour = moment().hour();
 
-      // loop over time blocks
       $(".time-block").each(function () {
           var blockHour = parseInt($(this).attr("id").split("hour")[1]);
           console.log( blockHour, currentHour)
 
-          //check if we've moved past this time
-          if (blockHour < currentHour) {
+       if (blockHour < currentHour) {
               $(this).addClass("past");
               $(this).removeClass("future");
               $(this).removeClass("present");
